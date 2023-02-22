6c783db0fbe61b80dd7072f9f087f022d86b09a7
Ankita Bharat Badhe <113541646+ankitabadhe@users.noreply.github.com>
2023-02-22 20:07:15 +0530

    Add files via upload

---
+++ cir.js
@@ -0,0 +1,5 @@
+exports.message="Circle"
+exports.area=function(r){return Math.PI*r*r;}
+exports.peri=function(r){return 2*Math.PI*r;}
+
+	
---
+++ circle.js
@@ -0,0 +1,5 @@
+
+var dt = require("./cir.js");
+console.log(dt.message);
+console.log("Area of a circle with radius 10= "+dt.area(10));
+console.log("Perimeter of a circle with radius 6= "+dt.peri(6));
